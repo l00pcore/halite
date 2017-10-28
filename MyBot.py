35114a95cb57011c73aa4268577e649dddb92410
crawforc3 <crawforc3@gmail.com>
2017-10-28 12:59:53 -0700

    Added simple logger to see ship statuses in the post-game

--- MyBot.py
+++ MyBot.py
@@ -8,11 +8,19 @@ import hlt
 # Then let's import the logging module so we can print out information
 import logging
 
+
+def ship_status():
+        logging.info("\n\nShip " + str(ship.id)
+                     + "\n" + "    health: " + str(ship.health)
+                     + "\n" + "    " + str(ship.docking_status)
+                     + "\n" + "    planet: " + str(ship.planet)
+                     + "\n\n")
+
 # GAME START
 # Here we define the bot's name as Settler and initialize the game, including communication with the Halite engine.
 game = hlt.Game("loopcore")
 # Then we print our start message to the logs
-logging.info("Starting lpc bot!")
+logging.info("Starting loopcore bot!")
 
 while True:
     # TURN START
@@ -21,19 +29,30 @@ while True:
 
     # Here we define the set of commands to be sent to the Halite engine at the end of the turn
     command_queue = []
+
     # For every ship that I control
     for ship in game_map.get_me().all_ships():
-        # If the ship is docked
+
         if ship.docking_status != ship.DockingStatus.UNDOCKED:
             # Skip this ship
+            ship_status()
             continue
 
+        entities_by_distance = game_map.nearby_entities_by_distance(ship)
+        nearest_planet = None
+        for distance in sorted(entities_by_distance):
+            nearest_planet = next((nearest_entity for nearest_entity in entities_by_distance[distance] if
+                                   isinstance(nearest_entity, hlt.entity.Planet)), None)
+            if nearest_planet:
+                break
+
         # For each planet in the game (only non-destroyed planets are included)
+        # Change this to each planet in the list of nearest planets
         for planet in game_map.all_planets():
             # If the planet is owned
             if planet.is_owned():
                 # Skip this planet
-                # ###################### Right now only one ship docks, we need to dock multiple ships. like 5 or 10 maybe?
+
                 continue
 
             # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
@@ -52,8 +71,9 @@ while True:
                 navigate_command = ship.navigate(
                     ship.closest_point_to(planet),
                     game_map,
-                    speed=int(hlt.constants.MAX_SPEED/1),
+                    speed=int(hlt.constants.MAX_SPEED/1.5),
                     ignore_ships=True)
+                #logging.info("ship " + str(ship.id) + " --navigating")
                 # If the move is possible, add it to the command_queue (if there are too many obstacles on the way
                 # or we are trapped (or we reached our destination!), navigate_command will return null;
                 # don't fret though, we can run the command again the next turn)
@@ -61,7 +81,11 @@ while True:
                     command_queue.append(navigate_command)
             break
 
+        ship_status()
+
+
     # Send our set of commands to the Halite engine for this turn
     game.send_command_queue(command_queue)
+    logging.info("--"*50)
     # TURN END
 # GAME END
